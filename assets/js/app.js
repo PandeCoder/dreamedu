e8b5d627a39daa47aed829abb2e285795ab4696b
PandeCoder <business.crewweb@gmail.com>
2021-12-03 09:30:37 +0700

    landing page

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+// Nav Open
+$("#nav-toggler").on("click", function () {
+  $(".nav-lists").toggleClass("open-nav");
+  $(".nav-lists").toggleClass("close-nav");
+  $(".nav-brand").toggleClass("ml-24");
+  $(".nav-burger").toggleClass("mr-24");
+});
