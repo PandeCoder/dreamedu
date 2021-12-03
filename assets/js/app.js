3acd2aafae4f2ba30a49e088f47f192ca4cb4fd4
PandeCoder <business.crewweb@gmail.com>
2021-12-03 14:27:29 +0700

    mentor profile

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,8 @@ $("#nav-toggler").on("click", function () {
   $(".nav-brand").toggleClass("ml-24");
   $(".nav-burger").toggleClass("mr-24");
 });
+
+function redirectTo(page) {
+  if (page == "register") {
+  }
+}
